8ff2fb1e1875e8b880b9ad33e73163071a59a442
kulikaa <kulikaa@pik-industry.ru>
2023-08-23 16:03:02 +0300

    Первая команда, "работающее" приложение

---
+++ KpblcCadSample/CadCommands/AboutCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autodesk.AutoCAD.Runtime;
+
+namespace KpblcCadSample.CadCommands
+{
+    public static class AboutCommand
+    {
+        [CommandMethod("kpblc-about")]
+        public static void AboutCadCommand()
+        {
+            MessageBox.Show("Тип о программе", "KpblcCadSample", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
